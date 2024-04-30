1a5d1bc545bc2ab4579cbef75ca55f09a050a742
Roman Borzenkov <ramzes84kha@gmail.com>
2024-04-30 09:38:16 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config();
+var app = express();
+
+const multer = require('multer');
+
+// Set up storage for uploaded files
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => 
+  {    
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => 
+  {    
+    cb(null, file.originalname);
+  }
+});
+// Create the multer instance
+const upload = multer({ storage: storage });
+
+//module.exports = upload;
+
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => 
+{  
+  // Handle the uploaded file
+
+  //console.log(req.file, req.body)
+  //{"name":"testAppConsole.runtimeconfig.json","type":"application/json","size":268}
+  //console.log("fn " + fileName + " " + "fs " + fileSize );
+  res.json({ name:req.file.originalname,type:req.file.mimetype,size:req.file.size });
+});
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
